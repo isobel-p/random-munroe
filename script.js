0f6c7260083136b862e1b741987ae94205e9f6a4
Isobel Pang <173300585+isobel-p@users.noreply.github.com>
2025-07-31 15:31:27 +0100

    Create rectangles on click, add win criteria
    whoops this went from an xkcd tribute to a balancing game! poc, still wip

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // module aliases
-var Engine = Matter.Engine,
+let Engine = Matter.Engine,
     Render = Matter.Render,
     Runner = Matter.Runner,
     Bodies = Matter.Bodies,
@@ -8,14 +8,14 @@ var Engine = Matter.Engine,
     MouseConstraint = Matter.MouseConstraint;
 
 // create an engine
-var engine = Engine.create();
+let engine = Engine.create();
 world = engine.world;
 
 let w = window.innerWidth;
 let h = window.innerHeight;
 
 // create a renderer
-var render = Render.create({
+let render = Render.create({
     element: document.body,
     engine: engine,
     options: {
@@ -25,23 +25,29 @@ var render = Render.create({
     }
 });
 
+let rectangles = [
+    Bodies.rectangle(w/2, h/2, 100, 100),
+    Bodies.rectangle(w/2, h/2, 100, 100),
+    Bodies.rectangle(w/2, h/2, 100, 100),
+    Bodies.rectangle(w/2, h/2, 100, 100),
+]
+
+// walls!
 Composite.add(world, [
-    // walls!
     Bodies.rectangle(5, h/2, 10, h, {isStatic: true}),
     Bodies.rectangle(w-5, h/2, 10, h, {isStatic: true}),
-    Bodies.rectangle(w/2, 5, w, 10, {isStatic: true}),
+    Bodies.rectangle(w/2, 5, w, 10, {isStatic: true}), 
     Bodies.rectangle(w/2, h-5, w, 10, {isStatic: true}),
-
-    // test blocks
-    Bodies.rectangle(w/2, h/2, 100, 100),
-    Bodies.rectangle(w/2, h/2, 100, 100),
-    Bodies.rectangle(w/2, h/2, 100, 100),
-    Bodies.rectangle(w/2, h/2, 100, 100),
 ])
 
+
+for (let i=0; i<rectangles.length; i++) {
+    Composite.add(world, rectangles[i])
+}
+
 // add mouse control
-var mouse = Mouse.create(render.canvas)
-var mouseConstraint = MouseConstraint.create(engine, {
+let mouse = Mouse.create(render.canvas)
+let mouseConstraint = MouseConstraint.create(engine, {
         mouse: mouse,
         constraint: {
             stiffness: 0.1,
@@ -50,6 +56,12 @@ var mouseConstraint = MouseConstraint.create(engine, {
             }
         }
     });
+
+document.addEventListener("contextmenu", function() {
+    rect = Bodies.rectangle(mouse.position.x, mouse.position.y, 100, 100)
+    Composite.add(world, rect)
+    rectangles.push(rect)
+});
 
 Composite.add(world, mouseConstraint);
 
@@ -60,7 +72,20 @@ render.mouse = mouse;
 Render.run(render);
 
 // create runner
-var runner = Runner.create();
+let runner = Runner.create();
 
 // run the engine
 Runner.run(runner, engine);
+
+
+setInterval(function() {
+    for (let i=0; i<rectangles.length; i++) {
+        if (rectangles[i].position.y <= 100 && rectangles[i].speed < 0.01) {
+            console.log(rectangles[i].position.y)
+            console.log(rectangles[i].speed)
+            alert("You win!\nYour score was:" + rectangles.length)
+        }
+    }
+    console.log(rectangles[rectangles.length-1].position.y)
+    // console.log(rectangles[rectangles.length-1].speed)
+}, 500);
